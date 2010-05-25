9cff76b18dfb8f31899308c8aaccfc2b1f91f331
azucenabv <azucenabv@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-25 06:35:35 +0000

    Subida inicial

---
+++ src/ArticuloAlquilado.java
@@ -0,0 +1,81 @@
+import java.util.Date;
+
+public class ArticuloAlquilado {
+	private int codigo;
+	private Articulo articulo;
+	private Date fechaAlquiler;
+	private Date fechaDevolucion;
+	private Cliente cliente;
+	private String observaciones;
+	private int recargo;
+	private int precio;
+	private int tiempo;
+	
+	public ArticuloAlquilado(int codigo,Articulo articulo,Date fechaAlquiler,Date fechaDevolucion,Cliente cliente,String observaciones,
+			int recargo,int precio,int tiempo){
+		this.codigo = codigo;
+		this.articulo = articulo;
+		this.fechaAlquiler = fechaAlquiler;
+		this.fechaDevolucion = fechaDevolucion;
+		this.cliente = cliente;
+		this.observaciones = observaciones;
+		this.recargo = recargo;
+		this.precio = precio;
+		this.tiempo = tiempo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+	public Articulo getArticulo() {
+		return articulo;
+	}
+	public void setFechaAlquiler(Date fechaAlquiler) {
+		this.fechaAlquiler = fechaAlquiler;
+	}
+	public Date getFechaAlquiler() {
+		return fechaAlquiler;
+	}
+	public void setFechaDevolucion(Date fechaDevolucion) {
+		this.fechaDevolucion = fechaDevolucion;
+	}
+	public Date getFechaDevolucion() {
+		return fechaDevolucion;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setRecargo(int recargo) {
+		this.recargo = recargo;
+	}
+	public int getRecargo() {
+		return recargo;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+	public int getTiempo() {
+		return tiempo;
+	}
+}
