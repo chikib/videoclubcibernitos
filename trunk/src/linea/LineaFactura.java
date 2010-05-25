fd95abec0e85f903de99c3d2c08b4623caa44fca
magg1982 <magg1982@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-25 11:10:02 +0000

    estoy añadiendo las clases usuario , line,lineafactura y lineaalbaran

---
+++ trunk/src/linea/LineaFactura.java
@@ -0,0 +1,16 @@
+package linea;
+
+public class LineaFactura {
+
+	private int recargo;
+
+	public int getRecargo() {
+		return recargo;
+	}
+
+	public void setRecargo(int recargo) {
+		this.recargo = recargo;
+	}
+	
+	
+}
