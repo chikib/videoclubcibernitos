af9a73544fee5f14aba3648f6523fd9dfc992f54
azucenabv <azucenabv@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-29 21:14:57 +0000

    Añadido lo necesario para el alquiler, devolución y gestión de películas

--- src/articulos/Articulo.java
+++ src/articulos/Articulo.java
@@ -1,9 +1,13 @@
 package articulos;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import bbdd.Articulos;
+import bbdd.ArticulosAlquilados;
 
 import varios.InputStreamVideoclub;
 import varios.VideoException;
@@ -151,17 +155,7 @@ public class Articulo {
 	public static Articulo buscaArticulo(int codigo){
 		int i = 0;
 		Articulos artbbdd = new Articulos();
-		Articulo art = artbbdd.buscarArticuloCodigoBarras(codigo);
-		/*for (Articulo articulo : listaPeliculas)
-		{
-			if (articulo.getCodigoBarras() == codigo)
-			{
-				//Videoclub.listaUsuarios.indexOf(us);
-				return i; // devuelve la posicion en que se encuentra el cliente dentro de la lista
-			}
-			i++;
-		}*/
-		
+		Articulo art = artbbdd.buscarArticuloCodigoBarras(codigo);		
 		return art;
 	}
 	
@@ -202,12 +196,12 @@ public class Articulo {
 			switch(codigo){
 				case 0: Videoclub.mostrarMenuPrincipal();break;
 				default: {
-						art = buscaArticulo(codigo);
-						if(art!=null){
-							return art;
-						}else{
-							throw new VideoException("*** El número de película indicada no existe ***\n");
-						}
+					art = buscaArticulo(codigo);
+					if(art!=null){
+						return art;
+					}else{
+						throw new VideoException("*** El número de película indicada no existe ***\n");
+					}
 				}
 			}
 		}
@@ -218,5 +212,335 @@ public class Articulo {
 			System.out.println(e.getMessage());
 		}
 		return art;
+	}
+	
+	public String rellenaPelicula()
+	{
+		String mensaje = "";
+		try{
+			String cadena = "";
+			InputStreamVideoclub.pedirCadena(" Introducir título : ");
+			cadena = InputStreamVideoclub.cadena;
+			setTitulo(cadena);
+		
+			InputStreamVideoclub.pedirCadena(" Introducir descripción : ");
+			cadena = InputStreamVideoclub.cadena;				
+			setDescripcion(cadena);
+							
+			InputStreamVideoclub.pedirCadena(" Introducir precio del alquiler : ");
+			cadena = InputStreamVideoclub.cadena;
+			setPrecioAlquiler(Double.parseDouble(cadena));
+			
+			setAlquilado(false);
+			
+			InputStreamVideoclub.pedirCadena(" Indicar si es alquilable (0 para falso, 1 para cierto): ");
+			cadena = InputStreamVideoclub.cadena;
+			setAlquilable(cadena.equals("1"));
+			
+			InputStreamVideoclub.pedirCadena(" Introducir el precio de compra : ");
+			cadena = InputStreamVideoclub.cadena;
+			setPrecioCompra(Double.parseDouble(cadena));
+			boolean error = false;
+			do{
+				error = false;
+				InputStreamVideoclub.pedirCadena(" Introducir la fecha de compra (dd/mm/yyyy) : ");
+				cadena = InputStreamVideoclub.cadena;
+				DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+				try{
+					setFechaCompra(sdf.parse(cadena));
+				}catch(ParseException e){
+					System.out.println(e.getMessage());
+					error = true;
+				}
+			}while(error);
+			
+			InputStreamVideoclub.pedirCadena(" Indicar si es una novedad (0 para falso, 1 para cierto): ");
+			cadena = InputStreamVideoclub.cadena;				
+			setNovedad(cadena.equals("1"));
+			
+			InputStreamVideoclub.pedirCadena(" Introducir localización : ");
+			cadena = InputStreamVideoclub.cadena;				
+			setLocalizacion(cadena);
+			
+			InputStreamVideoclub.pedirCadena(" Introducir código de barras : ");
+			cadena = InputStreamVideoclub.cadena;				
+			setCodigoBarras(Long.parseLong(cadena));
+			
+			Proveedor prov = new Proveedor();
+			prov.imprimirProveedores();
+			InputStreamVideoclub.pedirCadena("Elija uno de los siguientes proveedores: ");
+			cadena = InputStreamVideoclub.cadena;				
+			prov.setCodigo(Integer.parseInt(cadena));
+			setProveedor(prov);
+			
+			Soporte soporte = new Soporte();
+			soporte.imprimirSoportes();
+			InputStreamVideoclub.pedirCadena("Elija uno de los siguientes soportes: ");
+			cadena = InputStreamVideoclub.cadena;				
+			soporte.setCodigo(Integer.parseInt(cadena));
+			setSoporte(soporte);
+			
+			Categoria categoria = new Categoria();
+			categoria.imprimirCategorias();
+			InputStreamVideoclub.pedirCadena("Elija una de las siguientes categorías: ");
+			cadena = InputStreamVideoclub.cadena;				
+			categoria.setCodigo(Integer.parseInt(cadena));
+			setCategoria(categoria);
+		}catch(NumberFormatException e){
+			mensaje = "Ha ocurrido un error al transformar uno de los valores indicados en la inserción";
+		}
+		return mensaje;
+	}	
+	
+	public void menuMostrarCampos(){
+		String sNovedad = "0";
+		if(isNovedad()){
+			sNovedad = "1";
+		}
+		
+		String sAlquilado = "0";
+		if(isAlquilado()){
+			sAlquilado = "1";
+		}
+		
+		String sAlquilable = "0";
+		if(isAlquilable()){
+			sAlquilable = "1";
+		}
+		
+		System.out.println("La lista de campos que se pueden modificar son: ");
+		System.out.println("(1) Título: "+getTitulo());
+		System.out.println("(2) Descripción: "+getDescripcion());
+		System.out.println("(3) Categoría: "+getCategoria().getCodigo()+", "+getCategoria().getTematica());
+		new Categoria().imprimirCategorias();
+		System.out.println("(4) Soporte: "+getSoporte().getCodigo()+", "+getSoporte().getTipo());
+		new Soporte().imprimirSoportes();
+		System.out.println("(5) Proveedor: "+getProveedor().getCodigo()+", "+getProveedor().getNombre());
+		new Proveedor().imprimirProveedores();
+		System.out.println("(6) Precio de alquiler: "+getPrecioAlquiler());
+		System.out.println("(7) Alquilado: "+sAlquilado);
+		System.out.println("(8) Fecha de compra: "+getFechaCompra());
+		System.out.println("(9) Precio de compra: "+getPrecioCompra());
+		System.out.println("(10) Novedad: "+sNovedad);
+		System.out.println("(11) Localización: "+getLocalizacion());
+		System.out.println("(12) Código de barras: "+getCodigoBarras());
+		System.out.println("(13) Alquilable: "+sAlquilable);
+		System.out.println("(14) Salir ");
+	}
+	
+	public void menuModificarPelicula(){
+		int opcion ;
+		boolean salir = false;
+		//mostrar los campos de ese artículo y sus valores
+		try{
+			do{
+				InputStreamVideoclub.pedirCadena(" Indica el campo que desea modificar o la opción indicada para terminar: ");
+				opcion = Integer.parseInt(InputStreamVideoclub.cadena);
+				switch(opcion){
+					case 1: {//titulo
+						InputStreamVideoclub.pedirCadena("Nuevo valor para el título: ");
+						String titulo = InputStreamVideoclub.cadena;
+						setTitulo(titulo);
+						break;
+					}case 2:{//descripcion
+						InputStreamVideoclub.pedirCadena("Nuevo valor para la descripción: ");
+						String descripcion = InputStreamVideoclub.cadena;
+						setDescripcion(descripcion);
+						break;
+					}case 3:{//categoria
+						InputStreamVideoclub.pedirCadena("Nuevo valor para la categoría: ");
+						String categoria = InputStreamVideoclub.cadena;
+						Categoria cat = new Categoria();
+						cat.setCodigo(Integer.parseInt(categoria));
+						setCategoria(cat);
+						break;
+					}case 4:{//soporte
+						InputStreamVideoclub.pedirCadena("Nuevo valor para el soporte: ");
+						String sSoporte = InputStreamVideoclub.cadena;
+						Soporte soporte = new Soporte();
+						soporte.setCodigo(Integer.parseInt(sSoporte));
+						setSoporte(soporte);
+						break;
+					}case 5:{//proveedor
+						InputStreamVideoclub.pedirCadena("Nuevo valor para el proveedor: ");
+						String proveedor = InputStreamVideoclub.cadena;
+						Proveedor prov = new Proveedor();
+						prov.setCodigo(Integer.parseInt(proveedor));
+						setProveedor(prov);
+						break;
+					}case 6:{//precioAlquiler
+						InputStreamVideoclub.pedirCadena("Nuevo valor para el precio de alquiler: ");
+						String precio = InputStreamVideoclub.cadena;
+						setPrecioAlquiler(Double.parseDouble(precio));
+						break;
+					}case 7:{//alquilado
+						InputStreamVideoclub.pedirCadena("Nuevo valor para la opción de alquilado (Indicar 1 para cierto y 0 para falso): ");
+						String alquilado = InputStreamVideoclub.cadena;
+						setAlquilado(alquilado.equals("1"));
+					}case 8:{//fechaCompra
+						try{
+							InputStreamVideoclub.pedirCadena("Nuevo valor para la fecha de compra (Formato dd/mm/yyyy): ");
+							DateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+							Date fechaCompra = sdf.parse(InputStreamVideoclub.cadena);
+							setFechaCompra(fechaCompra);
+						}catch(ParseException e){
+							System.out.println("Error en el formato de la fecha");
+						}
+						break;
+					}case 9:{//precioCompra
+						InputStreamVideoclub.pedirCadena("Nuevo valor para el precio de compra: ");
+						String precio = InputStreamVideoclub.cadena;
+						setPrecioCompra(Double.parseDouble(precio));
+						break;
+					}case 10:{//novedad
+						InputStreamVideoclub.pedirCadena("Nuevo valor para la opción de novedad (Indicar 1 para cierto y 0 para falso): ");
+						String alquilado = InputStreamVideoclub.cadena;
+						setAlquilado(alquilado.equals("1"));
+						break;
+					}case 11:{//localizacion
+						InputStreamVideoclub.pedirCadena("Nuevo valor para la localización: ");
+						String localizacion = InputStreamVideoclub.cadena;
+						setTitulo(localizacion);
+						break;
+					}case 12:{//codigoBarras
+						InputStreamVideoclub.pedirCadena("Nuevo valor para el código de barras: ");
+						String codigoBarras = InputStreamVideoclub.cadena;
+						setTitulo(codigoBarras);
+						break;
+					}case 13:{
+						InputStreamVideoclub.pedirCadena("Nuevo valor para la opción de alquilable (Indicar 1 para cierto y 0 para falso): ");
+						String alquilable = InputStreamVideoclub.cadena;
+						setAlquilable(alquilable.equals("1"));
+						break;
+					}case 14:{
+						//Confirmar que los datos finales son los que desea guardar
+						Articulos artBbdd = new Articulos();
+						artBbdd.editar(this);
+						salir= true; 
+						break;
+					}default:{
+						System.out.println("** Opción incorrecta **");
+					}
+				}
+			}while(!salir);
+		}catch(NumberFormatException e){
+			System.out.println("Error en el formato de la opción");
+			menuModificarPelicula();
+		}
+	}
+	
+	public String rellenaPeliculaEdicion()
+	{
+		String mensaje = "";
+		try{
+			String cadena = "";
+			InputStreamVideoclub.pedirCadena(" Introducir título : ");
+			cadena = InputStreamVideoclub.cadena;
+			setTitulo(cadena);
+		
+			InputStreamVideoclub.pedirCadena(" Introducir descripción : ");
+			cadena = InputStreamVideoclub.cadena;				
+			setDescripcion(cadena);
+							
+			InputStreamVideoclub.pedirCadena(" Introducir precio del alquiler : ");
+			cadena = InputStreamVideoclub.cadena;
+			setPrecioAlquiler(Double.parseDouble(cadena));
+			
+			setAlquilado(false);
+			
+			InputStreamVideoclub.pedirCadena(" Indicar si es alquilable (0 para falso, 1 para cierto): ");
+			cadena = InputStreamVideoclub.cadena;
+			setAlquilable(cadena.equals("1"));
+			
+			InputStreamVideoclub.pedirCadena(" Introducir el precio de compra : ");
+			cadena = InputStreamVideoclub.cadena;
+			setPrecioCompra(Double.parseDouble(cadena));
+			boolean error = false;
+			do{
+				error = false;
+				InputStreamVideoclub.pedirCadena(" Introducir la fecha de compra (dd/mm/yyyy) : ");
+				cadena = InputStreamVideoclub.cadena;
+				DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+				try{
+					setFechaCompra(sdf.parse(cadena));
+				}catch(ParseException e){
+					System.out.println(e.getMessage());
+					error = true;
+				}
+			}while(error);
+			
+			InputStreamVideoclub.pedirCadena(" Indicar si es una novedad (0 para falso, 1 para cierto): ");
+			cadena = InputStreamVideoclub.cadena;				
+			setNovedad(cadena.equals("1"));
+			
+			InputStreamVideoclub.pedirCadena(" Introducir localización : ");
+			cadena = InputStreamVideoclub.cadena;				
+			setLocalizacion(cadena);
+			
+			InputStreamVideoclub.pedirCadena(" Introducir código de barras : ");
+			cadena = InputStreamVideoclub.cadena;				
+			setCodigoBarras(Long.parseLong(cadena));
+			
+			Proveedor prov = new Proveedor();
+			prov.imprimirProveedores();
+			InputStreamVideoclub.pedirCadena("Elija uno de los siguientes proveedores: ");
+			cadena = InputStreamVideoclub.cadena;				
+			prov.setCodigo(Integer.parseInt(cadena));
+			setProveedor(prov);
+			
+			Soporte soporte = new Soporte();
+			soporte.imprimirSoportes();
+			InputStreamVideoclub.pedirCadena("Elija uno de los siguientes soportes: ");
+			cadena = InputStreamVideoclub.cadena;				
+			soporte.setCodigo(Integer.parseInt(cadena));
+			setSoporte(soporte);
+			
+			Categoria categoria = new Categoria();
+			categoria.imprimirCategorias();
+			InputStreamVideoclub.pedirCadena("Elija una de las siguientes categorías: ");
+			cadena = InputStreamVideoclub.cadena;				
+			categoria.setCodigo(Integer.parseInt(cadena));
+			setCategoria(categoria);
+		}catch(NumberFormatException e){
+			mensaje = "Ha ocurrido un error al transformar uno de los valores indicados en la inserción";
+		}
+		return mensaje;
+	}
+	
+	public String insertar(){
+		String mensaje = "";
+		Articulos art = new Articulos();
+		int res = art.insertar(this);
+		if(res==0){
+			mensaje = "No se ha podido insertar el artículo";
+		}else{
+			System.out.println("\n******** Artículo insertado *********\n");
+		}
+		return mensaje;
+	}
+	
+	public String editar(){
+		String mensaje = "";
+		Articulos art = new Articulos();
+		int res = art.editar(this);
+		if(res==0){
+			mensaje = "No se ha podido modificar el artículo";
+		}else{
+			System.out.println("\n******** Artículo modificado *********\n");
+		}
+		return mensaje;
+	}
+	
+	public String modificarActivacion(boolean estado){
+		String mensaje = "";
+		Articulos art = new Articulos();
+		setAlquilable(estado);
+		int res = art.modificarActivacion(this);
+		if(res==0){
+			mensaje = "No se ha podido modificar el estado del artículo";
+		}else{
+			System.out.println("\n******** Artículo modificado *********\n");
+		}
+		return mensaje;
 	}
 }
--- src/articulos/ArticuloAlquilado.java
+++ src/articulos/ArticuloAlquilado.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import bbdd.Articulos;
+import bbdd.ArticulosAlquilados;
 import bbdd.Categorias;
 import usuario.Usuario;
 
@@ -16,13 +17,14 @@ public class ArticuloAlquilado {
 	private Usuario cliente;
 	private String observaciones;
 	private int recargo;
-	private int precio;
+	private double precio;
 	private int tiempo;
 	
 	public static List<ArticuloAlquilado> listaPeliculasAlquiladas = new ArrayList();
 	
 	public ArticuloAlquilado(){
-		
+		this.articulo = new Articulo();
+		this.cliente = new Usuario();
 	}
 	
 	public ArticuloAlquilado(Usuario usuario, Articulo articulo){
@@ -31,8 +33,20 @@ public class ArticuloAlquilado {
 	}
 	
 	public ArticuloAlquilado(int codigo,Articulo articulo,Date fechaAlquiler,Date fechaDevolucion,Usuario cliente,String observaciones,
-			int recargo,int precio,int tiempo){
+			int recargo,double precio,int tiempo){
 		this.codigo = codigo;
+		this.articulo = articulo;
+		this.fechaAlquiler = fechaAlquiler;
+		this.fechaDevolucion = fechaDevolucion;
+		this.cliente = cliente;
+		this.observaciones = observaciones;
+		this.recargo = recargo;
+		this.precio = precio;
+		this.tiempo = tiempo;
+	}
+	
+	public ArticuloAlquilado(Articulo articulo,Date fechaAlquiler,Date fechaDevolucion,Usuario cliente,String observaciones,
+			int recargo,double precio,int tiempo){
 		this.articulo = articulo;
 		this.fechaAlquiler = fechaAlquiler;
 		this.fechaDevolucion = fechaDevolucion;
@@ -85,10 +99,10 @@ public class ArticuloAlquilado {
 	public int getRecargo() {
 		return recargo;
 	}
-	public void setPrecio(int precio) {
+	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	public int getPrecio() {
+	public double getPrecio() {
 		return precio;
 	}
 	public void setTiempo(int tiempo) {
@@ -98,21 +112,21 @@ public class ArticuloAlquilado {
 		return tiempo;
 	}
 	
-	public static void alquilar(){
+	public static String alquilar(){
+		String mensaje = "";
 		//Identifico al usuario
 		Usuario usuario = Usuario.identificarUsuario();
 		//Identifico la película
 		if(usuario!=null){
 			Articulo articulo = Articulo.identificarPelicula();
-			if(articulo!=null){
-				//Articulo articulo = Articulo.listaPeliculas.get(posArticulo);
+			if(articulo!=null && !articulo.isAlquilado() && articulo.isAlquilable()){
 				int recargo = 0;
 				int tiempo = 0;
+				//Recupero la categoría de base de datos
 				Categorias categoriaBbdd = new Categorias();
 				Categoria cat = categoriaBbdd.buscarCategoriaCodigo(articulo.getCategoria().getCodigo());
 				if(cat!=null){
 					articulo.setCategoria(cat);
-					//Recupero la categoría de base de datos
 					if(articulo.isNovedad()){
 						recargo = cat.getRecargoNovedad();
 						tiempo = cat.getTiempoAlquilerNovedad();
@@ -120,42 +134,80 @@ public class ArticuloAlquilado {
 						recargo = cat.getRecargoBase();
 						tiempo = cat.getTiempoAlquiler();
 					}
-					//listaPeliculasAlquiladas.add(new ArticuloAlquilado(ArticuloAlquilado.generarCodigo(), articulo, new Date(),null , 
-					//		usuario, "", recargo, articulo.getPrecioAlquiler(), tiempo));
-					articulo.setAlquilado(true);
-					
+					//Creo una relación entre el artículo y el cliente
+					ArticuloAlquilado artAlq = new ArticuloAlquilado(articulo,new Date(),null,usuario,"",recargo,articulo.getPrecioAlquiler(),tiempo);
+					ArticulosAlquilados artAlqBbdd = new ArticulosAlquilados();
 					//Guardar cambio en bbdd
-					//Articulo.listaPeliculas.set(posArticulo, articulo);
-					
-					System.out.println("*** Artículo alquilado ***");
+					Articulos artBbdd = new Articulos();
+					//Establezco que el artículo está alquilado
+					articulo.setAlquilado(true);
+					if(artBbdd.setArticuloAlquilado(articulo)==1){
+						if(artAlqBbdd.insertar(artAlq)==1){
+							System.out.println("*** Artículo alquilado ***\n");
+						}else{
+							//Si la isnerción en articulos alquilados da error doy marcha atrás en que el artículo está alquilado
+							articulo.setAlquilado(false);
+							artBbdd.setArticuloAlquilado(articulo);
+							mensaje = "Ha ocurrido un error al asignar el artículo al cliente";
+						}
+					}else{
+						mensaje = "Ha habido un problema al establecer que el artículo ha sido alquilado";
+					}
+				}else{
+					mensaje = "Ha habido un problema al identificar la categoría del artículo";
 				}
+			}else{
+				mensaje = "El artículo no existe o ya está alquilado";
 			}
+		}else{
+			mensaje = "Ha habido un error al identificar el usuario";
 		}
+		return mensaje;
 	}
 	
-	
-	public static void gestionDevolver(){
+	public static String gestionDevolver(){
+		String mensaje = "";
 		//Identifico al usuario
 		Usuario usuario = Usuario.identificarUsuario();
 		if(usuario!=null){
 			//Identifico la película
-			/*this. = Articulo.identificarPelicula();
-			
-			//Articulo articulo = Articulo.listaPeliculas.get(posArticulo);
-			//ArticuloAlquilado articuloDev = buscar(usuario, articulo);
-			art.setAlquilado(false);
-			articuloDev.setFechaDevolucion(new Date());
-			Articulo.listaPeliculas.set(posArticulo, articulo);
-			System.out.println("*** Artículo devuelto ***");*/
 			Articulo articulo = Articulo.identificarPelicula();
-			articulo.setAlquilado(false);
-			Articulos artBbdd = new Articulos();
-			//artBbdd.guardar(articulo);
-			ArticuloAlquilado articuloDev = new ArticuloAlquilado();
-			articuloDev.setFechaDevolucion(new Date());
-			
-			System.out.println("*** Artículo devuelto ***");
+			if(articulo!=null && articulo.isAlquilado()){
+				//Identifico el artículo alquilado en base de datos
+				ArticulosAlquilados artDevBbdd = new ArticulosAlquilados();
+				ArticuloAlquilado articuloDev = new ArticuloAlquilado();
+				articuloDev = artDevBbdd.consultaArtAlqCodigo(usuario.getCodigo(),articulo.getCodigo());
+				if(articuloDev!=null){
+					articuloDev.setArticulo(articulo);
+					articuloDev.setCliente(usuario);
+					articulo.setAlquilado(false);
+					Articulos artBbdd = new Articulos();
+					//Cambio el atributo alquilado a falso
+					if(artBbdd.setArticuloAlquilado(articulo)==1){
+						//Actualizo la fecha de devolución
+						articuloDev.setFechaDevolucion(new Date());
+						if(artDevBbdd.actualizarFechaDevolucion(articuloDev)==1){
+							//Creo la factura y calculo si hay recargo
+							System.out.println("*** Artículo devuelto ***");
+							return "";
+						}else{
+							articulo.setAlquilado(true);
+							artBbdd.setArticuloAlquilado(articulo);
+							mensaje = "Se ha producido un error al establecer la fecha de devolución";
+						}
+					}else{
+						mensaje = "Se ha producido un error al establecer que el artículo ha sido alquilado";
+					}
+				}else{
+					mensaje = "El cliente indicado no tiene ese artículo alquilado";
+				}
+			}else{
+				mensaje = "El artículo no existe o no está alquilado";
+			}
+		}else{
+			mensaje = "El usuario no ha sido encontrado";
 		}
+		return mensaje;
 	}
 	
 	public static int generarCodigo(){
