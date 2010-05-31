1a6f1f6afd9f72f0800872c5e78606e98853f28a
azucenabv <azucenabv@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-31 08:03:02 +0000

    Se añaden métodos que devuelven las lineas por código o por el objeto que las contienen

--- trunk/src/bbdd/LineasFactura.java
+++ trunk/src/bbdd/LineasFactura.java
@@ -1,4 +1,10 @@
 package bbdd;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import articulos.ArticuloAlquilado;
+import facturas.Factura;
 import facturas.LineaFactura;
 
 public class LineasFactura {
@@ -11,4 +17,54 @@ public class LineasFactura {
 		con.cerrarConexion();
 		return res;
 	}	
+	
+	public LineaFactura consultarLineaFactura(int codigo){
+		Conexion con = new Conexion();
+		LineaFactura lf = null;
+		try{
+			ResultSet rs = con.consulta("select codigo,articuloAlquilado,factura,precio,recargo from " +
+					"lineasFactura where codigo = "+codigo);
+			if(rs.next()){
+				lf = new LineaFactura();
+				lf.setCodigo(rs.getInt("codigo"));
+				ArticuloAlquilado artAlq = new ArticuloAlquilado();
+				artAlq.setCodigo(rs.getInt("articuloAlquilado"));
+				lf.setArtAlq(artAlq);
+				Factura factura = new Factura();
+				factura.setCodigo(rs.getInt("factura"));
+				lf.setFactura(factura);
+				lf.setPrecio(rs.getInt("precio"));
+				lf.setRecargo(rs.getInt("recargo"));
+			}
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		return lf;
+	}
+	
+	public List<LineaFactura> consultarLineaFacturaPorFactura(int codFactura){
+		Conexion con = new Conexion();
+		List<LineaFactura> listaLineas = new ArrayList<LineaFactura>();
+		LineaFactura lf = null;
+		try{
+			ResultSet rs = con.consulta("select codigo,articuloAlquilado,factura,precio,recargo from " +
+					"lineasFactura where codigo = "+codFactura);
+			while(rs.next()){
+				lf = new LineaFactura();
+				lf.setCodigo(rs.getInt("codigo"));
+				ArticuloAlquilado artAlq = new ArticuloAlquilado();
+				artAlq.setCodigo(rs.getInt("articuloAlquilado"));
+				lf.setArtAlq(artAlq);
+				Factura factura = new Factura();
+				factura.setCodigo(rs.getInt("factura"));
+				lf.setFactura(factura);
+				lf.setPrecio(rs.getInt("precio"));
+				lf.setRecargo(rs.getInt("recargo"));
+				listaLineas.add(lf);
+			}
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		return listaLineas;
+	}
 }
