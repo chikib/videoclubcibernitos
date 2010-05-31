34da69bdad13621b7cc42088f4c2e4291702d6ca
azucenabv <azucenabv@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-31 12:13:36 +0000

    Se añade la funcionalidad de proveedores

--- src/bbdd/Proveedores.java
+++ src/bbdd/Proveedores.java
@@ -37,5 +37,77 @@ public class Proveedores {
 		return listaProveedores;
 	}
 	
+	public Proveedor buscarProveedor(int codigo){
+		Conexion con = new Conexion();
+		Proveedor prov = null;
+		try{
+			ResultSet res = con.consulta("select codigo,nombre,cif,telefono,direccion,web,fax from proveedores " +
+					"where codigo = "+codigo);
+			if(res.next()){
+				prov = new Proveedor();
+				prov.setCodigo(res.getInt("codigo"));
+				prov.setNombre(res.getString("nombre"));
+				prov.setCif(res.getString("cif"));
+				prov.setTelefono(res.getString("telefono"));
+				prov.setDireccion(res.getString("direccion"));
+				prov.setWeb(res.getString("web"));
+				prov.setFax(res.getString("fax"));
+			}
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		return prov;
+	}
+	
+	public Proveedor buscarProveedorCif(String cif){
+		Conexion con = new Conexion();
+		Proveedor prov = null;
+		try{
+			ResultSet res = con.consulta("select codigo,nombre,cif,telefono,direccion,web,fax from proveedores " +
+					"where cif = '"+cif+"'");
+			if(res.next()){
+				prov = new Proveedor();
+				prov.setCodigo(res.getInt("codigo"));
+				prov.setNombre(res.getString("nombre"));
+				prov.setCif(res.getString("cif"));
+				prov.setTelefono(res.getString("telefono"));
+				prov.setDireccion(res.getString("direccion"));
+				prov.setWeb(res.getString("web"));
+				prov.setFax(res.getString("fax"));
+			}
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		return prov;
+	}
+	
+	public int crearProveedor(Proveedor prov){
+		int res = 0;
+		Conexion con = new Conexion();
+		res = con.insertarUpdate("insert into proveedores(nombre,cif,telefono,direccion,web,fax)" +
+				"values('"+prov.getNombre()+"','"+prov.getCif()+"'," +
+				"'"+prov.getTelefono()+"','"+prov.getDireccion()+"','"+prov.getWeb()+"','"+prov.getFax()+"')");
+		con.cerrarConexion();
+		return res;
+	}
+	
+	public int modificarProveedor(Proveedor prov){
+		int res = 0;
+		Conexion con = new Conexion();
+		res = con.insertarUpdate("update proveedores set nombre='"+prov.getNombre()+"',cif='"+prov.getCif()+"'" +
+				", telefono='"+prov.getTelefono()+"', direccion='"+prov.getDireccion()+"',web='"+prov.getWeb()+"'" +
+				", fax='"+prov.getFax()+"' where codigo = "+prov.getCodigo());
+		con.cerrarConexion();
+		return res;
+	}
+	
+	public int eliminarProveedor(Proveedor prov){
+		int res = 0;
+		Conexion con = new Conexion();
+		res = con.insertarUpdate("delete from proveedores where codigo = "+prov.getCodigo());
+		con.cerrarConexion();
+		return res;
+	}
+	
 	
 }
