74466a217c7175d558607eea7e6a8e8f4f53b093
twister88888 <twister88888@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-31 12:02:40 +0000

    Actualizacion albaran y usuarios, penultimo dia

--- trunk/src/facturas/Albaran.java
+++ trunk/src/facturas/Albaran.java
@@ -39,12 +39,28 @@ public class Albaran extends Caja {
 		
 	}
 	
-	public void buscarAlbaran(int codAl){
-		Albaranes bddAl = new Albaranes();
-		bddAl.buscarAlbaran(codAl);
+	public String  alPantalla ()
+	{		
+		Usuario usu = Usuario.buscaUscod(getCliente().getCodigo());
+		return ("Código:         " + getCodigo() + "\n" +
+		"Precio total:    " + getPrecioTotal() + "\n" +
+		"Fecha:      " + getFecha() + "\n" +
+		"Cliente:     " + usu.getNombre() + " " + usu.getApellidos()+ "\n" +
+		"Cancelado:   " + isCancelado()+ "\n"); 
+				
 	}
 	
-	//Consulta los albaranes en un mes concreto
+	public Albaran buscarAlbaran(int codAl){
+		return new Albaranes().buscarAlbaranDatos(codAl);
+	}
+	
+	public List buscarAlbaranFechas(int mes,int ano,int rango){
+		List<Albaran> listaAlbaran = new ArrayList();
+		new Albaranes().buscarAlbaranFechas(mes,ano,rango);
+		return listaAlbaran;
+	}
+
+	//Consulta los albaranes en un mes y año concreto
 	public List consultar(int mes){
 		List miLista = new ArrayList();
 		/*for (Albaran o : listaAlbaranes){
@@ -57,28 +73,28 @@ public class Albaran extends Caja {
 
 	
 	//Consulta los albaranes entre dos fechas
-	public List consultaFechas(String fecha1, String fecha2){
+	/*public List consultaFechas(String fecha1, String fecha2){
 		miAlbaran.buscarAlbaranFechas(fecha1,fecha2);
 		
 		
 		
-		/*for (Albaran o : listaAlbaranes){
+		for (Albaran o : listaAlbaranes){
 			long ffactura=o.getFecha().getTime();
 			if (ffactura>=f1 && ffactura<=f2)
 			{
 				miLista.add(o);
 			}
 		}
-		return miLista;*/
+		return miLista;
 	
 		
 		return new ArrayList();
-	}
+	}*/
 	
-	public String cancelarAlbaran()
+	public String cancelarAlbaran(int cod)
 	{
 		String mensaje="";
-		int res=miAlbaran.cancelacionAlbaran(this);
+		int res=miAlbaran.cancelacionAlbaran(cod);
 		
 		if (res==0){
 			mensaje="No se ha podido cancelar el albaran";
