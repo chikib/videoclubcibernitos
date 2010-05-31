cab16139f00de3232cc70e9353721753094cf353
azucenabv <azucenabv@90e7ea04-1129-7447-cc95-909bb8d0f945>
2010-05-31 08:03:02 +0000

    Se añaden métodos que devuelven las lineas por código o por el objeto que las contienen

--- src/bbdd/LineasAlbaran.java
+++ src/bbdd/LineasAlbaran.java
@@ -1,5 +1,12 @@
 package bbdd;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import articulos.ArticuloAlquilado;
+import facturas.Albaran;
 import facturas.LineaAlbaran;
 
 public class LineasAlbaran {
@@ -12,4 +19,58 @@ public class LineasAlbaran {
 		con.cerrarConexion();
 		return res;
 	}	
+	
+	public LineaAlbaran consultarLineaAlbaran(int codigo){
+		Conexion con = new Conexion();
+		LineaAlbaran la = null;
+		try{
+			ResultSet rs = con.consulta("select codigo, articuloAlquilado,albaran,precio from" +
+					"lineasFactura where codigo = "+codigo);
+			if(rs.next()){
+				la = new LineaAlbaran();
+				la.setCodigo(rs.getInt("codigo"));
+				ArticuloAlquilado artAlq = new ArticuloAlquilado();
+				artAlq.setCodigo(rs.getInt("articuloAlquilado"));
+				la.setArtAlq(artAlq);
+				Albaran albaran = new Albaran();
+				albaran.setCodigo(rs.getInt("albaran"));
+				la.setAlbaran(albaran);
+				la.setPrecio(rs.getInt("precio"));
+			}
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		finally{
+			con.cerrarConexion();
+		}
+		return la;
+	}
+	
+	public List<LineaAlbaran> consultarLineaAlbaranPorAlbaran(int codAlbaran){
+		Conexion con = new Conexion();
+		List<LineaAlbaran> listaLineas = new ArrayList<LineaAlbaran>();
+		LineaAlbaran la = null;
+		try{
+			ResultSet rs = con.consulta("select codigo, articuloAlquilado,albaran,precio from" +
+					"lineasFactura where albaran = "+codAlbaran);
+			while(rs.next()){
+				la = new LineaAlbaran();
+				la.setCodigo(rs.getInt("codigo"));
+				ArticuloAlquilado artAlq = new ArticuloAlquilado();
+				artAlq.setCodigo(rs.getInt("articuloAlquilado"));
+				la.setArtAlq(artAlq);
+				Albaran albaran = new Albaran();
+				albaran.setCodigo(rs.getInt("albaran"));
+				la.setAlbaran(albaran);
+				la.setPrecio(rs.getInt("precio"));
+				listaLineas.add(la);
+			}
+		}catch(SQLException e){
+			System.out.println(e.getMessage());
+		}
+		finally{
+			con.cerrarConexion();
+		}
+		return listaLineas;
+	}
 }
